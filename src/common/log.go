4a1aec1ff6bf0943910ee821ec5bb5911c2edb55
Anthony <cdistc@163.com>
2020-07-19 00:50:28 +0800

    ignore

---
+++ src/common/log.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"log"
+	"os"
+)
+
+func InitLogger(file string) {
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		panic(err)
+	}
+	// 将文件设置为log输出的文件
+	log.SetOutput(logFile)
+	//log.SetPrefix("[qSkipTool]")
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+}
