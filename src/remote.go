0faec96d9328363e329e4b386db04536f4f34876
Anthony <cdistc@163.com>
2020-07-27 02:22:59 +0800

    new

---
+++ src/remote.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func main() {
+	//var configPath string
+	//if len(os.Args) >= 2 {
+	//	configPath = os.Args[1]
+	//} else {
+	//	configPath = "config/"
+	//}
+	//log.Println("init server...")
+	//serverConfig = loadServerConfig(configPath)
+	//common.InitLogger(serverConfig.logFile)
+
+	http.HandleFunc("/", start)
+	log.Println("start server...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func start(w http.ResponseWriter, r *http.Request) {
+	//cmd := exec.Command("wakeonlan", "-i", "192.168.2.0 d0:50:99:70:87:3f")
+	cmd := exec.Command("ls", "-l", "/")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Execute Command failed:" + err.Error())
+		return
+	}
+	fmt.Println("Execute Command finished.")
+}
