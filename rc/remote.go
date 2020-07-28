1b1a5c36e79d1c27ec574528b393fa2cae0e0503
anthonychen <anthonychen@lexin.com>
2020-07-28 18:28:37 +0800

    finish remote controller

--- rc/remote.go
+++ rc/remote.go
@@ -2,31 +2,49 @@ package main
 
 import (
 	"bytes"
+	"domain/common"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"syscall"
 )
 
+type RemoteControlConfig struct {
+	Port    string
+	LogFile string
+	Client  struct {
+		Ip  string
+		Mac string
+	}
+}
+type config struct {
+	RemoteControl RemoteControlConfig `mapstructure:"remote"`
+}
+
+var remoteControlConfig RemoteControlConfig
+
 func main() {
-	//var configPath string
-	//if len(os.Args) >= 2 {
-	//	configPath = os.Args[1]
-	//} else {
-	//	configPath = "config/"
-	//}
-	//log.Println("init server...")
-	//serverConfig = loadServerConfig(configPath)
-	//common.InitLogger(serverConfig.logFile)
+	var configPath string
+	if len(os.Args) >= 2 {
+		configPath = os.Args[1]
+	} else {
+		configPath = "config/"
+	}
+	log.Println("init server...")
+	remoteControlConfig = loadRemoteConfig(configPath)
+	common.InitLogger(remoteControlConfig.LogFile)
 
 	http.HandleFunc("/", start)
 	log.Println("start server...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+remoteControlConfig.Port, nil))
 }
 
 func start(w http.ResponseWriter, r *http.Request) {
-	command := exec.Command("/bin/sh", "-c", "wakeonlan -i 192.168.2.0 d0:50:99:70:87:3f")
+	cmd := "wakeonlan -i " + remoteControlConfig.Client.Ip + " " + remoteControlConfig.Client.Mac
+
+	command := exec.Command("/bin/sh", "-c", cmd)
 	outinfo := bytes.Buffer{}
 	command.Stdout = &outinfo
 	err := command.Start()
@@ -40,4 +58,12 @@ func start(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(command.ProcessState.Sys().(syscall.WaitStatus).ExitStatus())
 		fmt.Println(outinfo.String())
 	}
+}
+
+func loadRemoteConfig(configPath string) RemoteControlConfig {
+	var config = config{}
+	v, _ := common.Load(configPath)
+	v.Unmarshal(&config)
+	log.Println(config)
+	return config.RemoteControl
 }
